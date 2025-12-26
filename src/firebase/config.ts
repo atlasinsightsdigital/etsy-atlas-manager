c1c8d190c095d284894922a2b757f285458a1b81
Grow Together <atlasinsightsdigital@gmail.com>
2025-12-26 00:07:50 +0000

    Move project files to root directory

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-5135010085-b15f6",
-  "appId": "1:1061930338910:web:28b14039ab82607228c2cf",
-  "apiKey": "AIzaSyDBuqROJUr8sRXcBMZEPHIs5vi88_OtphU",
-  "authDomain": "studio-5135010085-b15f6.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1061930338910"
-};
